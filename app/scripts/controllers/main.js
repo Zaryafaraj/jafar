818f8740ae12730dad7d0aaa80c5a033ad1c2301
Fathalian <fatalian@uw.edu>
2015-01-20 13:09:04 -0800

    Added the initial data structure for topics

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,57 @@
  */
 angular.module('jafarApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.topics = [
+      {
+        title : 'Design',
+        image: 'images/design.png',
+        link: '#/topics/design'
+      },
+      {
+        title : 'Cars',
+        image: 'images/cars.jpg',
+        link: '#/topics/cars'
+      },
+      {
+        title : 'Gaming',
+        image: 'images/gaming.png',
+        link: '#/topics/gaming'
+      },
+      {
+        title : 'Science',
+        image: 'images/science.jpg',
+        link: '#/topics/science'
+      },
+      {
+        title : 'Sports',
+        image: 'images/sports.png',
+        link: '#/topics/sports'
+      },
+      {
+        title : 'Finance',
+        image: 'images/finance.png',
+        link: '#/topics/finance'
+      },
+      {
+        title : 'Travel',
+        image: 'images/travel.jpg',
+        link: '#/topics/travel'
+      },
+      {
+        title : 'Finance',
+        image: 'images/finance.png',
+        link: '#/topics/finance'
+      },
+      {
+        title : 'Cinema',
+        image: 'images/cinema.jpeg',
+        link: '#/topics/cinema'
+      },
+      {
+        title : 'Tech',
+        image: 'images/technology.png',
+        link: '#/topics/tech'
+      },
     ];
   });
+
