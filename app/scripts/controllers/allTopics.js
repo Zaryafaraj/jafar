5e4d7de4c3102e558933c63aca711c487a9d5481
Zarya Faraj <zaryafaraj@gmail.com>
2015-07-06 01:01:26 -0700

    fixed a bug related untidy links

--- app/scripts/controllers/allTopics.js
+++ app/scripts/controllers/allTopics.js
@@ -11,6 +11,7 @@ angular.module('jafarApp').controller('AllTopicCtrl',
 
             allTopicNews.categories.query().$promise.then(function (result) {
                 $scope.allTopicNews = result;
+
                 $scope.topNews = {};
                 _.each($scope.allTopicNews, function (topic) {
                     topicNews.topic.get({topicId: topic}).$promise.then(function (topicNews) {
@@ -19,7 +20,7 @@ angular.module('jafarApp').controller('AllTopicCtrl',
                             //if we haven't filled up the headlines and the item has an author that is not unknown and the item has a unique thumbnail
                             //save it to headlines
                             if (res.length < 5 && item.author && item.author !== 'unknown') {
-                                if(item.link.indexOf('coolhunting') === -1 && item.link.indexOf('.') !== -1){
+                                if (item.link.indexOf('coolhunting') === -1 && item.link.indexOf('.') !== -1 && item.link.indexOf('http') === 0) {
                                     res.push(item);
                                 }
 
--- app/scripts/services/topicService.js
+++ app/scripts/services/topicService.js
@@ -6,30 +6,34 @@
 
 var app = angular.module('jafarApp.services');
 
-app.service( 'allTopicNews', ['$resource', function( $resource ) {
-  this.categories = $resource( 'http://farnsworth.zyring.com/categories' );
-}] );
+app.service('allTopicNews', ['$resource', function ($resource) {
+    this.categories = $resource('http://farnsworth.zyring.com/categories');
+}]);
 
-app.service( 'topicNews', ['$resource', function( $resource ) {
-  this.topic = $resource('http://farnsworth.zyring.com/categories/:topicId');
-}] );
-
+app.service('topicNews', ['$resource', function ($resource) {
+    this.topic = $resource('http://farnsworth.zyring.com/categories/:topicId');
+}]);
 
 
 app.factory('TopicLoader', ['topicNews', '$route', '$q',
-  function(topicNews, $route, $q){
+    function (topicNews, $route, $q) {
 
-    //constructor function used in resolve
-    return function() {
-      var topicId = $route.current.params.topicId;
-      var deferred = $q.defer();
-      topicNews.topic.get({topicId: topicId},
-        function( topic ) {
-          deferred.resolve( topic );
-        }, function() {
-          deferred.reject( 'Unable to fetch topic news' );
-        } );
-      return deferred.promise;
-    };
+        //constructor function used in resolve
+        return function () {
+            var topicId = $route.current.params.topicId;
+            var deferred = $q.defer();
+            topicNews.topic.get({topicId: topicId},
+            function (topic) {
+                topic.items = _.filter(topic.items, function(aTopic){
+                    return aTopic.link.indexOf('http') === 0 ;
+                    //console.log(aTopic);
+                });
+                deferred.resolve(topic);
+            },
+            function () {
+                deferred.reject('Unable to fetch topic news');
+            });
+            return deferred.promise;
+        };
 
-  }]);
+    }]);
